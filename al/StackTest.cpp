f96a4e9607ea203ef9e73481a78bf642133120c6
lihongyu <283576530@qq.com>
2019-10-12 19:12:31 +0800

    stack

--- al/StackTest.cpp
+++ al/StackTest.cpp
@@ -27,10 +27,9 @@ void TestStack()
 	std::cout << stack.ToString() << std::endl;
 }
 
-//表达式实现 四则运算 + - */ eg:a + b + c*d*f
-void TestExpression()
+
+int Expression(string str)
 {
-	string str = "1+2+5*4";
 	Stack stackOp;
 	Stack stackNum;
 
@@ -47,10 +46,27 @@ void TestExpression()
 				int value;
 				stackOp.Top(value);
 				char op = static_cast<char>(value);
+				if( c == '+')
+				{
+
+				}
+				else if( c == '-')
+				{
+
+				}
+				else if( c == '*')
+				{
+
+				}
+				else if( c == '/')
+				{
+
+				}
 			}
 		}
 		else if( c == '+' || c == '-' || c == '*' || c == '/' )
 		{
+
 		}
 		else
 		{
@@ -60,14 +76,89 @@ void TestExpression()
 		}
 	}
 
+	return 0;
+}
 
+//表达式实现 四则运算 + - */ eg:a + b + c*d*f
+void TestExpression1()
+{
+	string str = "1+2+5*4";
+	int result = Expression(str);
+
+	std::cout << "TestExpression1 " << result << endl ;
+}
+
+void TestExpression2()
+{
+	string str = "4/5+1+2+5*4";
+	int result = Expression(str);
+
+	std::cout << "TestExpression2 " << result << endl ;
+}
+
+static bool Macth(string str)
+{
+	Stack stack;
+
+	bool inputerror = false;
+	for(int i = 0; i < str.size(); i++)
+	{
+		char c = str[i];
+		if( c == '[' || c == '{' )
+		{
+			stack.Push(c);
+		} 
+		else if(c == ']' || c == '}')
+		{
+			if(stack.Count() <= 0)
+				return false;
+			int value;
+			stack.Top(value);
+			char ccmp = static_cast<char>(value);
+			if(c== ']' && ccmp == '[')
+			{
+				stack.Pop();
+			}
+			else if(c== '}' && ccmp == '{' )
+			{
+				stack.Pop();
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			inputerror = true;
+			break;
+		}
+		
+	}
+
+	return stack.Count() == 0;
+}
+
+//括号匹配 {}[]
+void TestMath1()
+{
+	string str = "{[{[[]]}]}";
+	bool matched = Macth(str);
+	if(matched)
+		std::cout << "TestMath1 matched " << endl;
+	else
+		std::cout << "TestMath1 match error" << endl;
 }
 
 
-//括号匹配 {}[]
-void TestMath()
+void TestMath2()
 {
-
+	string str = "{[}]";
+	bool matched = Macth(str);
+	if(matched)
+		std::cout << "TestMath1 matched " << endl;
+	else
+		std::cout << "TestMath1 match error" << endl;
 }
 
 
@@ -89,8 +180,10 @@ public:
 	void Register()
 	{
 		FuncMain::Instance()->AddMethod(TestStack);
-		FuncMain::Instance()->AddMethod(TestExpression);
-		FuncMain::Instance()->AddMethod(TestMath);
+		FuncMain::Instance()->AddMethod(TestExpression1);
+		FuncMain::Instance()->AddMethod(TestExpression2);
+		FuncMain::Instance()->AddMethod(TestMath1);
+		FuncMain::Instance()->AddMethod(TestMath2);
 		FuncMain::Instance()->AddMethod(TestDoRedoList);
 	}
 };
